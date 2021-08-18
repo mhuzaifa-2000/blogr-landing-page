30d9ec2fecfe4114a93726a85d8339a5290dee81
mhuzaifa-2000 <74556971+mhuzaifa-2000@users.noreply.github.com>
2021-08-18 20:18:26 +0500

    Update index.js

--- Fronted Challenge/index.js
+++ Fronted Challenge/index.js
@@ -9,3 +9,21 @@ window.addEventListener("scroll",()=>{
         nav[0].classList.remove("nav-hide");
     }
 })
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+document.querySelector(".container").addEventListener("click",()=>{
+    document.querySelector('.ham-menu').classList.toggle('active')   
+})
+
+document.querySelectorAll(".ham-item").forEach((item,index)=>{
+    item.addEventListener("click",()=>{
+        if(document.querySelectorAll('.submenu')[index].style.display=="block")
+        document.querySelectorAll('.submenu')[index].style.display="none";
+        else
+        document.querySelectorAll('.submenu')[index].style.display="block";
+
+    })
+    
+})
